179d9d9503a23a6b2d9ef310bb0da8be9bd0a0f3
Zane Othman <zane.othman@gmail.com>
2020-09-10 10:50:13 -0700

    Added basic socket code to listen for connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::net::{
+    TcpListener
+};
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:80")?;
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_) => {
+                println!("Received connection!");
+            },
+            Err(error) => {
+                println!("Error connecting: {}", error)
+            }
+
+        }
+    }
+
+    Ok(())
 }
