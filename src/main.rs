59d7634a6b48f7fb3d7cbb4fab27af411e2c99f2
Zane Othman <zane.othman@gmail.com>
2020-11-05 17:19:20 -0800

    Response now contains client's message
    The message can contain multiple lines and still be read fine.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
 use std::{
-    io::{self, Write},
+    io::{self, prelude::*, BufReader, BufWriter},
     net::TcpListener,
 };
 
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:80")?;
+    println!("Listening for connections!");
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 println!("Received connection!");
 
-                if let Ok(addr) = stream.peer_addr() {
-                    let msg_string = format!("Hello from flotsam! Your IP address is {}.", addr);
-                    stream.write_all(msg_string.as_bytes())?;
-                } else {
-                    stream.write_all(b"Hello from flotsam!")?;
+                // Read message from client, line by line
+                let mut reader = BufReader::new(stream);
+                let mut msg_buf = String::new();
+
+                while let Ok(num) = reader.read_line(&mut msg_buf) {
+                    // Break on empty line (\r\n)
+                    if num <= 2 {
+                        break;
+                    }
                 }
+
+                // Dispose of reader after reading entire message
+                stream = reader.into_inner();
+
+                // Create a BufWriter to write to the stream (response)
+                let mut response = BufWriter::new(stream);
+
+                // Hello message
+                response.write_all(b"Hello from flotsam! Your message was:\r\n")?;
+
+                // Respond with message back to client
+                response.write_all(msg_buf.as_bytes())?;
             }
             Err(error) => eprintln!("Error accepting connection: {}", error),
         }
