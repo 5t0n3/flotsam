6541cb997c42a5a41e90b3d1481336842e260178
Zane Othman <zane.othman@gmail.com>
2020-11-05 17:49:37 -0800

    Changed method for handling messages
    It is now refactored to the handle_connection function, which in turn
    takes another function to process the client's message

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,45 @@
 use std::{
     io::{self, prelude::*, BufReader, BufWriter},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
+
+fn handle_connection<H>(stream: TcpStream, handler: H) -> io::Result<()>
+where
+    H: FnOnce(String, BufWriter<TcpStream>) -> io::Result<()>,
+{
+    // Read message from client, line by line
+    let mut reader = BufReader::new(stream);
+    let mut msg_buf = String::new();
+
+    while let Ok(num) = reader.read_line(&mut msg_buf) {
+        // Break on empty line (\r\n)
+        if num <= 2 {
+            break;
+        }
+    }
+
+    // Dispose of reader after reading entire message
+    let write_stream = reader.into_inner();
+
+    // Create a BufWriter to write to the stream (response)
+    let response = BufWriter::new(write_stream);
+
+    // Pass control to handler function
+    handler(msg_buf, response)?;
+
+    Ok(())
+}
+
+// Chapter 4 handler
+fn respond_with_message(message: String, mut writer: BufWriter<TcpStream>) -> io::Result<()> {
+    // Hello message
+    writer.write_all(b"Hello from flotsam! Your message was:\r\n")?;
+
+    // Respond with message back to client
+    writer.write_all(message.as_bytes())?;
+
+    Ok(())
+}
 
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:80")?;
@@ -9,31 +47,9 @@ fn main() -> io::Result<()> {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("Received connection!");
-
-                // Read message from client, line by line
-                let mut reader = BufReader::new(stream);
-                let mut msg_buf = String::new();
-
-                while let Ok(num) = reader.read_line(&mut msg_buf) {
-                    // Break on empty line (\r\n)
-                    if num <= 2 {
-                        break;
-                    }
-                }
-
-                // Dispose of reader after reading entire message
-                stream = reader.into_inner();
-
-                // Create a BufWriter to write to the stream (response)
-                let mut response = BufWriter::new(stream);
-
-                // Hello message
-                response.write_all(b"Hello from flotsam! Your message was:\r\n")?;
-
-                // Respond with message back to client
-                response.write_all(msg_buf.as_bytes())?;
+                handle_connection(stream, respond_with_message)?;
             }
             Err(error) => eprintln!("Error accepting connection: {}", error),
         }
